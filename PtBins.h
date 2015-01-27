1ef54c0c58f4c33e3b63cb65143fa3a1847ecb59
Jian Sun <sun229@purdue.edu>
2015-01-27 17:10:59 -0500

    add rapidity bins

--- PtBins.h
+++ PtBins.h
@@ -1,9 +1,25 @@
-#define NPT  11
 //double ptbins[NPT+1] = {0,4,5,7,9,11,14,17,20,30,40,100};
 //double ptbins[NPT+1] = {0,3,3.5,4.0,4.5,5.5,7,9,11,13,16,20,28,40,100};
 //for analysis
+#define NPT  11
 double ptbins[NPT+1] = {0,4.5,5.5,7,9,11,13,16,20,28,40,100};
+
+
+//#define NY 12
+
+//double ybins[NY+1] = {-2.0, -1.8, -1.6, -1.2, -0.8, -0.4, 0, 0.4, 0.8, 1.2, 1.6, 1.8, 2.0};
+
+#define NY 6
+double ybins[NY+1] = {-2.0, -1.5, -0.7, 0, 0.7, 1.5, 2.0};
 
 //pp triger efficiency
 //#define NPT  16
 //double ptbins[NPT+1] = {0,4.5,5.5,7,9,11,13,16,18,20,22,25,30,35,40,45,60};
+//pPb trigger efficiency
+//#define NPT  14
+//double ptbins[NPT+1] = {0,4.5,5.5,7,9,11,13,16,18,20,22,24,28,40,100};
+
+//#define NPT 1
+//double ptbins[NPT+1] = {4.5, 40};
+//#define NY 1
+//double ybins[NY+1] = {-2.0, 2.0};
